193b3f68178d707833503091c06fdc3138e95458
Venirails <krishnaveni@RFL267.local>
2022-12-02 18:02:15 +0530

    first commit

---
+++ app/models/line_item.rb
@@ -0,0 +1,12 @@
+class LineItem < ApplicationRecord
+  belongs_to :cart
+  belongs_to :store
+
+
+  def total_price
+		store.price * quantity
+	end
+
+end
+
+
---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  resources :line_items
+  resources :carts
+  get 'gallery/index'
+  get 'gallery/checkout'
+  post 'gallery/checkout'
+
+  get 'gallery/search'
+  get 'gallery/purchase_complete'
+  resources :stores
+  get 'admin/login'
+  post 'admin/login'
+
+  get 'admin/logout'
+
+  devise_for :users
+  get 'home/index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
